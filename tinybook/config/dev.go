3195f73e0e0217d8152fe1658610007bcbf50ae6
ycvk <ycvk@icloud.com>
2023-11-03 13:01:04 +0800

    Fix: 移除db冗余的时区设置(已在mysql部署yaml中设置时区)

--- tinybook/config/dev.go
+++ tinybook/config/dev.go
@@ -4,7 +4,7 @@ package config
 
 var Config = config{
 	DB: DBConfig{
-		Host: "root:root@tcp(127.0.0.1:3308)/tinybook?charset=utf8mb4&parseTime=True&loc=Local&time_zone=Asia/Shanghai",
+		Host: "root:root@tcp(127.0.0.1:3308)/tinybook?charset=utf8mb4&parseTime=True&loc=Local",
 	},
 	Redis: RedisConfig{
 		Host: "localhost:6379",
--- tinybook/config/k8s.go
+++ tinybook/config/k8s.go
@@ -4,7 +4,7 @@ package config
 
 var Config = config{
 	DB: DBConfig{
-		Host: "root:root@tcp(tinybook-mysql:3308)/tinybook?charset=utf8mb4&parseTime=True&loc=Local&time_zone=Asia/Shanghai",
+		Host: "root:root@tcp(tinybook-mysql:3308)/tinybook?charset=utf8mb4&parseTime=True&loc=Local",
 	},
 	Redis: RedisConfig{
 		Host: "tinybook-redis:6380",
