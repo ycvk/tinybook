109aee23e516253cb6f9c00ab69b749602fc1a45
ycvk <ycvk@icloud.com>
2024-01-09 17:40:15 +0800

    Feat: 一致性hash环

--- tinybook/internal/job/ranking_job.go
+++ tinybook/internal/job/ranking_job.go
@@ -3,8 +3,11 @@ package job
 import (
 	"context"
 	"geek_homework/tinybook/internal/service"
+	"geek_homework/tinybook/pkg/hashring"
 	"github.com/bsm/redislock"
+	"github.com/lithammer/shortuuid/v4"
 	"go.uber.org/zap"
+	"math/rand"
 	"time"
 )
 
@@ -12,12 +15,58 @@ type RankingJob struct {
 	log        *zap.Logger
 	rankingSvc service.RankingService
 	time       time.Duration
-	redisLock  *redislock.Client
+	RedisLock  *redislock.Client
 	key        string
+
+	// TODO:这是用来模拟负载的，实际使用时需要删除
+	load      int32                    // 节点负载
+	threshold int32                    // 节点负载阈值
+	Id        string                   // 节点ID
+	hashRing  *hashring.ConsistentHash // 一致性哈希环
 }
 
-func NewRankingJob(rankingSvc service.RankingService, time time.Duration, lock *redislock.Client, l *zap.Logger) *RankingJob {
-	return &RankingJob{rankingSvc: rankingSvc, time: time, redisLock: lock, log: l}
+func NewRankingJob(rankingSvc service.RankingService, t time.Duration, lock *redislock.Client, l *zap.Logger) *RankingJob {
+	r := &RankingJob{
+		rankingSvc: rankingSvc,
+		time:       t,
+		RedisLock:  lock,
+		log:        l,
+		threshold:  85, // 节点负载阈值
+	}
+
+	// TODO:这是用来模拟负载的，实际使用时需要删除
+	uuid := shortuuid.New()
+	r.Id = uuid
+	ticker := time.NewTicker(time.Second) // 每秒钟更改一个负载 0-100 模拟负载
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				r.setLoad(rand.Int31n(100))
+			}
+		}
+	}()
+	// 初始化一致性哈希环
+	ch := &hashring.ConsistentHash{
+		Ring:  make(hashring.HashRing, 0),
+		Nodes: make(hashring.NodeMap),
+	}
+	// 添加节点
+	ch.AddNode(hashring.Node{ID: r.Id, Load: r.GetLoad()})
+	// 定时(10s)更新哈希环中的节点负载
+	ch.AutoUpdateLoadByFunc(r.Id, time.Second*10, r.GetLoad)
+	r.hashRing = ch
+	return r
+}
+
+// setLoad TODO:这是用来设置负载的，实际使用时需要删除
+func (r *RankingJob) setLoad(load int32) {
+	r.load = load
+}
+
+// GetLoad TODO:这是用来获取负载的，实际使用时需要删除
+func (r *RankingJob) GetLoad() int32 {
+	return r.load
 }
 
 func (r *RankingJob) Name() string {
@@ -25,9 +74,18 @@ func (r *RankingJob) Name() string {
 }
 
 func (r *RankingJob) Run() error {
+	node := r.hashRing.GetNode(string(rand.Int31())) // 获取负载极可能最小的节点
+	if node.Load > r.threshold {                     // 如果获取负载的节点也都已经负载超过阈值，那就暂不执行，打印日志
+		r.log.Warn("ranking job load is too high", zap.String("node", node.ID), zap.Int32("load", node.Load))
+		return nil
+	}
+	if node.ID != r.Id { // 如果不是自己，就不执行
+		return nil
+	}
+
 	timeout, c := context.WithTimeout(context.Background(), time.Second*3)
 	defer c()
-	lock, err := r.redisLock.Obtain(timeout, r.key, r.time,
+	lock, err := r.RedisLock.Obtain(timeout, r.key, r.time,
 		&redislock.Options{
 			RetryStrategy: redislock.LimitRetry(redislock.LinearBackoff(time.Millisecond*100), 3), // 重试3次，每次间隔100ms
 		})
@@ -43,7 +101,11 @@ func (r *RankingJob) Run() error {
 			r.log.Error("ranking job unlock failed", zap.Error(err2))
 		}
 	}()
-
+	// 执行前再次检查节点负载 如果目前节点突然已经超过阈值，就不执行，释放此分布式锁，打印日志
+	if r.GetLoad() > r.threshold {
+		r.log.Warn("ranking job load is too high", zap.String("node", node.ID), zap.Int32("load", node.Load))
+		return nil
+	}
 	ctx, cancelFunc := context.WithTimeout(context.Background(), r.time)
 	defer cancelFunc()
 	return r.rankingSvc.TopN(ctx)
---
+++ tinybook/pkg/hashring/hashring.go
@@ -0,0 +1,110 @@
+package hashring
+
+import (
+	"hash/crc32"
+	"sort"
+	"strconv"
+	"sync"
+	"time"
+)
+
+const MaxLoad = 100
+
+// Node 代表哈希环中的一个节点
+type Node struct {
+	ID   string
+	Load int32 // 节点负载
+}
+
+// HashRing 一致性哈希环
+type HashRing []uint32
+
+// NodeMap 节点映射表
+type NodeMap map[uint32]Node
+
+// ConsistentHash 一致性哈希结构体
+type ConsistentHash struct {
+	Ring  HashRing
+	Nodes NodeMap
+	sync.RWMutex
+}
+
+// AddNode 添加节点到哈希环
+func (c *ConsistentHash) AddNode(node Node) {
+	c.Lock()
+	defer c.Unlock()
+	virtualNodes := MaxLoad - node.Load // 虚拟节点数 负载越高的节点 虚拟节点越少 就越不容易被选中
+	for i := 0; i < int(virtualNodes); i++ {
+		key := c.hashKey(node.ID + strconv.Itoa(i))
+		c.Nodes[key] = node
+		c.Ring = append(c.Ring, key)
+	}
+	sort.Slice(c.Ring, func(i, j int) bool {
+		return c.Ring[i] < c.Ring[j]
+	})
+}
+
+// RemoveNode 从哈希环中删除节点
+func (c *ConsistentHash) RemoveNode(nodeID string) {
+	c.Lock()
+	defer c.Unlock()
+	for i := 0; i < MaxLoad; i++ {
+		key := c.hashKey(nodeID + strconv.Itoa(i))
+		index := c.search(key)
+		if index >= 0 {
+			c.Ring = append(c.Ring[:index], c.Ring[index+1:]...)
+			delete(c.Nodes, key)
+		}
+	}
+}
+
+// GetNode 返回负载极可能最小的节点
+func (c *ConsistentHash) GetNode(key string) Node {
+	c.RLock()
+	defer c.RUnlock()
+	if len(c.Ring) == 0 {
+		return Node{}
+	}
+	hash := c.hashKey(key)
+	idx := c.search(hash)
+	return c.Nodes[c.Ring[idx]]
+}
+
+// hashKey 为字符串键生成哈希值
+func (c *ConsistentHash) hashKey(key string) uint32 {
+	return crc32.ChecksumIEEE([]byte(key))
+}
+
+// search 查找哈希环中某个哈希值的索引
+func (c *ConsistentHash) search(hash uint32) int {
+	idx := sort.Search(len(c.Ring), func(i int) bool {
+		return c.Ring[i] >= hash
+	})
+	if idx == len(c.Ring) {
+		idx = 0
+	}
+	return idx
+}
+
+// UpdateLoad 更新节点负载
+func (c *ConsistentHash) UpdateLoad(nodeID string, load int32) {
+	c.Lock()
+	defer c.Unlock()
+	c.RemoveNode(nodeID)
+	node := Node{ID: nodeID, Load: load}
+	c.AddNode(node)
+}
+
+// AutoUpdateLoadByFunc 定时更新节点负载
+func (c *ConsistentHash) AutoUpdateLoadByFunc(nodeID string, duration time.Duration, loadFunc func() int32) {
+	ticker := time.NewTicker(duration)
+	go func() {
+		defer ticker.Stop()
+		for {
+			select {
+			case <-ticker.C:
+				c.UpdateLoad(nodeID, loadFunc())
+			}
+		}
+	}()
+}
