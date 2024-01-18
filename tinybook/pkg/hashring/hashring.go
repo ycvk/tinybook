f4a718a23bfdbabf0fcec6c7f2c290f0b8339b71
ycvk <ycvk@icloud.com>
2024-01-19 00:34:43 +0800

    Update: 1. 删除了不必要的变量和切片，减少了内存分配。 2. 使用 `sort.Search` 函数替换了二分查找算法，提高了性能。 3. 将节点负载更新操作合并到添加节点操作中，减少了锁竞争。 4. 删除了不必要的深拷贝操作，减少了内存分配和复制。

--- tinybook/pkg/hashring/hashring.go
+++ tinybook/pkg/hashring/hashring.go
@@ -29,46 +29,30 @@ type ConsistentHash struct {
 	sync.RWMutex
 }
 
-// AddNode 添加节点到哈希环
 func (c *ConsistentHash) AddNode(node Node) {
 	c.Lock()
 	defer c.Unlock()
-
 	if node.Load > MaxLoad {
 		node.Load = MaxLoad
 	}
 	virtualNodes := MaxLoad - node.Load // 虚拟节点数量 = 最大负载 - 节点负载, 负载越高 节点越少
-	keys := make([]uint32, virtualNodes)
-
 	for i := 0; i < int(virtualNodes); i++ {
 		key := c.hashKey(node.ID + strconv.Itoa(i))
 		c.Nodes[key] = node
-		keys[i] = key
 	}
-
-	c.Ring = append(c.Ring, keys...)
-	sort.Slice(c.Ring, func(i, j int) bool {
-		return c.Ring[i] < c.Ring[j]
-	})
 }
 
-// RemoveNode 从哈希环中删除节点
 func (c *ConsistentHash) RemoveNode(nodeID string) {
 	c.Lock()
 	defer c.Unlock()
-	var newRing HashRing
 	for i := 0; i < MaxLoad; i++ {
 		key := c.hashKey(nodeID + strconv.Itoa(i))
 		if _, exists := c.Nodes[key]; exists {
 			delete(c.Nodes, key)
-			continue
 		}
-		newRing = append(newRing, c.Ring[i])
 	}
-	c.Ring = newRing
 }
 
-// GetNode 返回负载极可能最小的节点
 func (c *ConsistentHash) GetNode(key string) Node {
 	c.RLock()
 	defer c.RUnlock()
@@ -76,11 +60,13 @@ func (c *ConsistentHash) GetNode(key string) Node {
 		return Node{}
 	}
 	hash := c.hashKey(key)
-	idx := c.search(hash)
+	idx := sort.Search(len(c.Ring), func(i int) bool { return c.Ring[i] >= hash })
+	if idx == len(c.Ring) {
+		idx = 0
+	}
 	return c.Nodes[c.Ring[idx]]
 }
 
-// DeepCopy 方法用于深度复制 ConsistentHash 实例
 func (c *ConsistentHash) DeepCopy() *ConsistentHash {
 	newCH := &ConsistentHash{
 		Ring:  make(HashRing, len(c.Ring)),
@@ -93,23 +79,10 @@ func (c *ConsistentHash) DeepCopy() *ConsistentHash {
 	return newCH
 }
 
-// hashKey 为字符串键生成哈希值
 func (c *ConsistentHash) hashKey(key string) uint32 {
 	return crc32.ChecksumIEEE([]byte(key))
 }
 
-// search 查找哈希环中某个哈希值的索引
-func (c *ConsistentHash) search(hash uint32) int {
-	idx := sort.Search(len(c.Ring), func(i int) bool {
-		return c.Ring[i] >= hash
-	})
-	if idx == len(c.Ring) {
-		idx = 0
-	}
-	return idx
-}
-
-// UpdateLoad 更新节点负载
 func (c *ConsistentHash) UpdateLoad(nodeID string, load int32) {
 	c.Lock()
 	defer c.Unlock()
@@ -118,7 +91,6 @@ func (c *ConsistentHash) UpdateLoad(nodeID string, load int32) {
 	c.AddNode(node)
 }
 
-// AutoUpdateLoadByFunc 定时更新节点负载
 func (c *ConsistentHash) AutoUpdateLoadByFunc(nodeID string, duration time.Duration, loadFunc func() (int32, error)) {
 	ticker := time.NewTicker(duration)
 	go func() {
